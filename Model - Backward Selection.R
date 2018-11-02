52ec10595447a5344b8de70497ce3e2e8480b48a
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2018-11-02 09:40:11 +0000

    model

--- Model - Backward Selection.R
+++ Model - Backward Selection.R
@@ -1,43 +1,43 @@
 #Backward Selection 
 #FullLinearModel
-BabiesDataNAOM<- na.omit(BabiesData)
-FullModel <- lm(wt ~ ., data = BabiesDataNAOM)
+
+FullModel <- lm(wt ~ ., data = babiesNoNA)
 summary(FullModel)
 
 Anova(FullModel)
 
 #Removing Variable with highest p value 
-Model<- update(FullModel, .~ . - dage)
+Model<- update(FullModel, .~ . - age)
 summary(Model)
 Anova(Model)
 #adjusted R squared increases 
 
-model1<- update(Model, .~. -marital)
+model1<- update(Model, .~. -race)
 summary(model1)
 Anova(model1)
 #adjusted R squared increases 
 
-model2<- update(model1,.~. -inc)
+model2<- update(model1,.~. -dage)
 summary(model2)
 Anova(model2)
 #adjusted R squared increases 
 
-model3 <- update(model2,.~. -med)
+model3 <- update(model2,.~. -marital)
 summary(model3)
 Anova(model3)
 #adjusted R squared increases 
 
-model4 <- update(model3,.~. -mage)
+model4 <- update(model3,.~. -ded)
 summary(model4)
 Anova(model4)
 #adjusted R squared increases 
 
-model5<- update(model4,.~. -mrace)
+model5<- update(model4,.~. -date)
 summary(model5)
 Anova(model5)
 #adjusted R squared increases 
 
-model6<-update(model5,.~. -mwt)
+model6<-update(model5,.~. -ed)
 summary(model6)
 Anova(model6)
 #adjusted R squared increases 
@@ -46,7 +46,7 @@ model7<- update(model6,.~. -smoke)
 summary(model7)
 Anova(model7)
 
-model8 <- update(model7, .~. -date)
+model8 <- update(model7, .~. -inc)
 summary(model8)
 Anova(model8)
 
@@ -62,3 +62,5 @@ finalmodel <- model9
 hist(finalmodel$residuals)
 shapiro.test(resid(finalmodel))
 plot(finalmodel)
+
+
